5851c4095d8a26d360c26277cefe9d97b7eab178
Envoyofhell <103904453+Envoyofhell@users.noreply.github.com>
2025-03-15 16:15:03 -0400

    Update initialize-document-event-listeners.js

--- client/src/initialization/document-event-listeners/initialize-document-event-listeners.js
+++ client/src/initialization/document-event-listeners/initialize-document-event-listeners.js
@@ -3,9 +3,17 @@ import { initializeSidebox } from './sidebox/initialize-sidebox.js';
 import { initializeTable } from './table/initialize-table.js';
 import { initializeWindow } from './window/window.js';
 
+let domEventListenersInitialized = false;
+
 export const initializeDOMEventListeners = () => {
+  if (domEventListenersInitialized) {
+    return; // Exit if already initialized
+  }
+
   initializeCardContextMenu();
   initializeSidebox();
   initializeTable();
   initializeWindow();
+
+  domEventListenersInitialized = true; // Mark as initialized
 };
